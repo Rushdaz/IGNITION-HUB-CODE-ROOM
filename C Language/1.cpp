7670a343172f3dcb4d71f0a807f0cabaf17f301a
Rushda fatima <rushdafatima916@gmail.com>
2024-11-13 22:00:57 +0530

    Update 1.cpp

--- C Language/1.cpp
+++ C Language/1.cpp
@@ -1,1 +1,18 @@
-#include 
+#include<stdio.h>
+int main()
+{
+  int a, b, c, big;
+printf("enter any three numbers: \n");
+scanf("%d%d%d",&a, &b, &c);
+big = a;
+if(b>big)
+{
+big = b;
+}
+if(c>big)
+{
+big = c;
+}
+printf("Largest of three number is : %d" , big);
+return 0;
+}
