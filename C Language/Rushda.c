c3f98dde5f26247a4b0ed53b7fe2ebd16cc81643
Rushda fatima <rushdafatima916@gmail.com>
2024-11-13 22:45:56 +0530

    Hollow square pattern with '*'. Based on the user's input

---
+++ C Language/Rushda.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() 
+
+{
+
+int i, j, n;
+
+printf ("enter the size"); 
+
+scanf("%d", &n);
+
+for (i=1 ; i<=n ; i++)
+
+{
+
+for ( j=1 ; j<=n ; j++)
+
+{
+
+if ((i==1) || (i==n) || (j==1) || (j==n))
+
+{
+
+printf("*");
+
+}
+
+else
+
+{
+
+printf(" ");
+
+}
+
+}
+
+printf("\n");
+
+}
+
+return 0;
+
+}
